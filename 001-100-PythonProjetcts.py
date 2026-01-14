f650f115a22d72822ab58924b850d97f961266ae
ABUNOW ROMANUS GIDEON OJONG <gideonseedboy@gmail.com>
2026-01-14 19:59:41 +0000

    Add files via upload

---
+++ 001-100-PythonProjetcts.py
@@ -0,0 +1,6 @@
+print("Welcome to 100 Python Projects");
+
+user_Name = input("Enter you name: ");
+user_Age = input("Enter your age: ");
+
+print("Hello " + user_Name + " ! you are " + user_Age + " years old. Welcom to Python programming. ")
