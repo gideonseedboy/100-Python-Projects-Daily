cb44d18763b54278de6aa5f3df388f4cc77584ad
ABUNOW ROMANUS GIDEON OJONG <gideonseedboy@gmail.com>
2026-01-15 03:11:34 +0000

    Add files via upload

---
+++ 005 - Simple Password Generator.py
@@ -0,0 +1,8 @@
+
+#  100 PYTHON PROJECTS — FROM BEGINNER TO ADVANCE WITH MySirGideon
+# PROJECT 5 — SIMPLE PASSWORD GENERATOR
+
+import random
+pword_length = input("How many characters should password have 12 - 24: ")
+
+random_pword = random.rand
